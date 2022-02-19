4358375bc3e07c9091084acc6ac3cc6aa454f221
Poonam Verma <poonamverma267@gmail.com>
2022-02-19 12:05:19 +0530

    dish details

--- src/components/PopularDishItem.js
+++ src/components/PopularDishItem.js
@@ -2,6 +2,7 @@
 const PopularDishItem = (props) => {
   const url =
     "http://63.142.251.101:3000/" + props.foodChef.dish_masters[0].dishImage;
+  const dish = props.foodChef.dish_masters[0].dishname;
   return (
     <li className="glide__slide slider">
       <img
@@ -11,7 +12,14 @@ const PopularDishItem = (props) => {
         width="300px"
         height="200px"
       />
-      {/* {props.foodChef.totalRating}({props.foodChef.reviewCnt} reviews) */}
+      <div className="dishContent">
+        <p className="dishTitle">{dish}</p>
+        <p className="dishChef">{props.foodChef.chefname}</p>
+        <p className="dishRating">
+          <span className="w3-badge w3-red">{props.foodChef.totalRating}</span>{" "}
+          ({props.foodChef.reviewCnt} reviews)
+        </p>
+      </div>
     </li>
   );
 };
